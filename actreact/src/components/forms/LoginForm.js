539951dfe625b5efd20858e11df0a1b79915fea1
AliAlrayan <126081192+AliAlrayan@users.noreply.github.com>
2023-12-09 15:33:21 +0800

    Add files via upload

---
+++ actreact/src/components/forms/LoginForm.js
@@ -0,0 +1,129 @@
+import { View, ToastAndroid, ImageBackground } from "react-native";
+import React from "react";
+import { Button, Text, TextInput, HelperText } from "react-native-paper";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import fetchServices from "../services/fetchServices";
+import backgroundImage from "./image/ace.jpg";
+
+export default function LoginForm({ navigation }) {
+  const [showPass, setShowPass] = React.useState(false);
+
+  const showToast = (message = "Something went wrong") => {
+    ToastAndroid.show(message, 3000);
+  };
+
+  const handleLogin = async (values) => {
+    try {
+      const url = "http://192.168.254.108(/api/v1/login";
+      const result = await fetchServices.postData(url, values);
+
+      if (result.message != null) {
+        showToast(result?.message);
+      } else {
+        navigation.navigate("Home");
+      }
+    } catch (e) {
+      console.debug(e.toString());
+    }
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid Email").required("Enter your email"),
+    password: Yup.string().required("Enter your password"),
+  });
+
+  return (
+    <ImageBackground source={backgroundImage} style={{ flex: 1, justifyContent: "center" }}>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={async (values) => {
+          await handleLogin(values);
+        }}
+        validationSchema={validationSchema}
+      >
+        {({
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          errors,
+          touched,
+          setTouched,
+        }) => {
+          return (
+            <View style={{ margin: 20 }}>
+              <Text style={{ fontSize: 32, marginBottom: 20, color: "white", fontWeight: "bold" }}>Login</Text>
+              <TextInput
+                mode="outlined"
+                placeholder="Email"
+                label="Email"
+                left={<TextInput.Icon icon="email" color="white" />}
+                style={{ marginTop: 10, backgroundColor: "transparent" }}
+                defaultValue={values.email}
+                value={values.email}
+                keyboardType="email-address"
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                error={errors.email && touched.email}
+                onFocus={() => setTouched({ email: true }, false)}
+                theme={{ colors: { primary: "white", text: "white" } }}
+              />
+              {errors.email && touched.email && (
+                <HelperText type="error" visible={errors.email}>
+                  {errors.email}
+                </HelperText>
+              )}
+              <TextInput
+                mode="outlined"
+                placeholder="Password"
+                label="Password"
+                left={<TextInput.Icon icon="lock" color="white" />}
+                secureTextEntry={!showPass}
+                right={
+                  <TextInput.Icon
+                    icon={showPass ? "eye" : "eye-off"}
+                    color="white"
+                    onPress={() => setShowPass(!showPass)}
+                  />
+                }
+                style={{ marginTop: 10, backgroundColor: "transparent" }}
+                value={values.password}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                error={errors.password && touched.password}
+                onFocus={() => setTouched({ password: true }, false)}
+                theme={{ colors: { primary: "white", text: "white" } }}
+              />
+              {errors.password && touched.password && (
+                <HelperText type="error" visible={errors.password}>
+                  {errors.password}
+                </HelperText>
+              )}
+              <Button
+                loading={isSubmitting}
+                disabled={isSubmitting}
+                onPress={handleSubmit}
+                icon="login"
+                mode="contained"
+                style={{ marginTop: 20, backgroundColor: "#f29c46" }}
+              >
+                Login
+              </Button>
+              <Button
+                disabled={isSubmitting}
+                onPress={() => navigation.navigate("Register")}
+                icon="account-plus"
+                mode="contained"
+                style={{ marginTop: 10, backgroundColor: "#f29c46" }}
+              >
+                Register
+              </Button>
+            </View>
+          );
+        }}
+      </Formik>
+    </ImageBackground>
+  );
+}
---
+++ actreact/src/components/forms/RegisterForm.js
@@ -0,0 +1,137 @@
+import { View, ToastAndroid, ImageBackground } from "react-native";
+import React from "react";
+import { Button, Text, TextInput } from "react-native-paper";
+import fetchServices from "../services/fetchServices";
+
+export default function RegisterForm({ navigation }) {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [repassword, setRepassword] = React.useState("");
+  const [showPass, setShowPass] = React.useState(false);
+  const [showRePass, setShowRePass] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  const showToast = (message = "Something went wrong") => {
+    ToastAndroid.show(message, ToastAndroid.SHORT);
+  };
+
+  const handleRegistration = async () => {
+    try {
+      setLoading(true);
+
+      if (name === "" || email === "" || password === "" || repassword === "") {
+        showToast("Input required data");
+        setIsError(true);
+        return false;
+      }
+
+      if (password !== repassword) {
+        showToast("Match the password");
+        setIsError(true);
+        return false;
+      }
+
+      const url = "http://192.168.254.108/api/v1/register";
+      const data = {
+        name,
+        email,
+        password,
+        password_confirmation: repassword,
+      };
+
+      const result = await fetchServices.postData(url, data);
+
+      if (result?.message != null) {
+        showToast(result?.message);
+      } else {
+        navigation.navigate("Login");
+      }
+    } catch (e) {
+      showToast(e.toString());
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <ImageBackground
+      source={require('./image/ace.jpg')}
+      style={{ flex: 1, resizeMode: 'cover', justifyContent: 'center' }}
+    >
+      <View style={{ flex: 1, padding: 20 }}>
+        <Text variant="displayMedium">Register</Text>
+        <TextInput
+          mode="outlined"
+          placeholder="Name"
+          label="Name"
+          style={{ marginTop: 10, backgroundColor: 'rgba(255,255,255,0.7)' }}
+          value={name}
+          onChangeText={setName}
+          error={isError}
+        />
+        <TextInput
+          mode="outlined"
+          placeholder="Email"
+          label="Email"
+          style={{ marginTop: 10, backgroundColor: 'rgba(255,255,255,0.7)' }}
+          value={email}
+          onChangeText={setEmail}
+          error={isError}
+        />
+        <TextInput
+          mode="outlined"
+          placeholder="Password"
+          label="Password"
+          secureTextEntry={!showPass}
+          right={
+            <TextInput.Icon
+              icon={showPass ? "eye" : "eye-off"}
+              onPress={() => setShowPass(!showPass)}
+            />
+          }
+          style={{ marginTop: 10, backgroundColor: 'rgba(255,255,255,0.7)' }}
+          value={password}
+          onChangeText={setPassword}
+          error={isError}
+        />
+        <TextInput
+          mode="outlined"
+          placeholder="Re-type Password"
+          label="Re-type Password"
+          secureTextEntry={!showRePass}
+          right={
+            <TextInput.Icon
+              icon={showPass ? "eye" : "eye-off"}
+              onPress={() => setShowRePass(!showRePass)}
+            />
+          }
+          style={{ marginTop: 10, backgroundColor: 'rgba(255,255,255,0.7)' }}
+          value={repassword}
+          onChangeText={setRepassword}
+          error={isError}
+        />
+        <Button
+          disabled={loading}
+          loading={loading}
+          icon="account-plus"
+          mode="contained"
+          style={{ marginTop: 10, backgroundColor: '#f29c46' }}
+          onPress={handleRegistration}
+        >
+          Register
+        </Button>
+        <Button
+          disabled={loading}
+          onPress={() => navigation.pop()}
+          icon="arrow-left"
+          mode="contained"
+          style={{ marginTop: 10, backgroundColor: '#f29c46' }}
+        >
+          Go Back
+        </Button>
+      </View>
+    </ImageBackground>
+  );
+}
